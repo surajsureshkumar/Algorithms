6e64d2b0df7668791ca30ac4551db44151378604
surajsureshkumar <surajsureshkumar.98@gmail.com>
2025-01-16 10:01:37 -0800

    OOP Implementation of Linear Search

--- Linear_Search.py
+++ Linear_Search.py
@@ -1,14 +1,19 @@
 class LinearSearch:
+    """This Class represents the OOP implementation of the Linear Search Algorithm"""
     def __init__(self, array: list):
         self.array = sorted(array)
 
-    def search(self, target):
-        pass
+    def search(self, target_value):
+        for value in range(len(self.array)):
+            if self.array[value] == target_value:
+                return value
+        return -1
 
 
 def main():
     sorted_array = [1,2,3,4,5,6,7,8]
     target_value = 7
+
     linear_search = LinearSearch(sorted_array)
     index_of_number = linear_search.search(target_value)
 
