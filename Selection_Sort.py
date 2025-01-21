09cd800b4d21d64cab892f71c97eb38bd461b255
surajsureshkumar <surajsureshkumar.98@gmail.com>
2025-01-21 11:17:41 -0800

    Class Implementation

---
+++ Selection_Sort.py
@@ -0,0 +1,11 @@
+class SelectionSort:
+    def __init__(self):
+        pass
+
+    
+def main():
+    array = [3,5,7,56,33,2]
+    selction_sort = SelectionSort()
+
+if __name__ == "__main__":
+    main()
