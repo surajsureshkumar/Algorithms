d68f3c904e9407c2f359e9f7b417762c33e99a6d
surajsureshkumar <surajsureshkumar.98@gmail.com>
2025-01-22 14:13:03 -0800

    Initial Commit

---
+++ Insertion_Sort.py
@@ -0,0 +1,15 @@
+class InsertionSort:
+    def __init__(self, array: list):
+        self.array = array
+
+    def insertion_sort(self):
+        pass
+
+def main():
+    array = []
+    algo = InsertionSort(array)
+    sorted_array = algo.insertion_sort()
+    print(sorted_array)
+
+if __name__ == "__main__":
+    main() 
