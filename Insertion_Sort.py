401347bf0c229b4589a6cc15db6f4659b57caa7b
surajsureshkumar <surajsureshkumar.98@gmail.com>
2025-01-29 13:31:23 -0800

    Insertion Sort

--- Insertion_Sort.py
+++ Insertion_Sort.py
@@ -3,10 +3,19 @@ class InsertionSort:
         self.array = array
 
     def insertion_sort(self):
-        pass
-
+        for idx in range(1,len(self.array)):
+            current_element = self.array[idx]
+            previous_element = idx - 1
+            
+            while previous_element >= 0 and self.array[previous_element] > current_element:
+                self.array[previous_element + 1] = self.array[previous_element]
+                previous_element -= 1
+            
+            self.array[previous_element + 1] = current_element
+        return self.array
+        
 def main():
-    array = []
+    array = [2,6,5,1,4,3]
     algo = InsertionSort(array)
     sorted_array = algo.insertion_sort()
     print(sorted_array)
