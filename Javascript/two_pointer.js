0b72ba87e67f3210803c55763d5c40d2065951c6
surajsureshkumar <surajsureshkumar.98@gmail.com>
2025-02-13 12:57:56 -0800

    Two Pointer Pattern

---
+++ Javascript/two_pointer.js
@@ -0,0 +1,12 @@
+function TwoPointer(){
+    // leftPointer to keep track of the left element in the array and the rightPointer to keep track of the rightmost element, you can also make sure that the right pointer starts at certain position.
+    let leftPointer = 0;
+    let rightPointer = Array.length;
+
+    while(leftPointer <= rightPointer){
+        // perform some operation based on the required condition
+        // increment left or decrement as required
+    }
+
+    // return the desired result here
+}
