844ca007ec09eeceecc9d73f5971fe53314e0a85
surajsureshkumar <surajsureshkumar.98@gmail.com>
2025-02-26 13:35:30 -0800

    skeleton

---
+++ Java/insertionSort.java
@@ -0,0 +1,12 @@
+class InsertionSort{
+
+    static int[] insertionSort(int [] array){
+        return array;
+    }
+
+    public static void main(String[] args) {
+        int [] array = {38,25,21,26,28};
+        InsertionSort obj = new InsertionSort();
+
+    }
+}
