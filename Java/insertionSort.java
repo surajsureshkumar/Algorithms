aa5113492b34097483f267b4e8c0905b86972841
surajsureshkumar <surajsureshkumar.98@gmail.com>
2025-02-26 15:18:04 -0800

    Insertion Sort implementation

--- Java/insertionSort.java
+++ Java/insertionSort.java
@@ -1,12 +1,25 @@
+import java.util.Arrays;
+
 class InsertionSort{
 
     static int[] insertionSort(int [] array){
+        for(int idx = 0; idx < array.length; idx++){
+            int currentElement = array[idx];
+            int previousElement = idx - 1;
+
+            while(previousElement >=0 && array[previousElement] > currentElement){
+                array[previousElement + 1] = array[previousElement];
+                previousElement --;
+            }
+            array[previousElement + 1] = currentElement;
+        }
         return array;
     }
 
     public static void main(String[] args) {
         int [] array = {38,25,21,26,28};
-        InsertionSort obj = new InsertionSort();
+        InsertionSort.insertionSort(array);
+        System.out.println("Sorted Array is "+ Arrays.toString(array));
 
     }
 }
