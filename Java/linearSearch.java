2bd4465054f05c40fcfab78960dae2c52c5a4686
surajsureshkumar <surajsureshkumar.98@gmail.com>
2025-02-26 15:13:51 -0800

    Corrected File Name to match with class

--- Java/linearSearch.java
+++ Java/linearSearch.java
@@ -10,7 +10,7 @@ public class LinearSearch{
     }
     
     public static void main(String[] args){
-        int [] array = {1,2,3,4,5,6,6,7}
+        int [] array = {1,2,3,4,5,6,6,7};
         int arrayLength = array.length;
         LinearSearch obj = new LinearSearch();
         int target = 5;
