dd4d4edb92124458d7309937d12ee643dbd89144
surajsureshkumar <surajsureshkumar.98@gmail.com>
2025-02-26 13:35:57 -0800

    Skeleton

---
+++ Java/mergeSort.java
@@ -0,0 +1,18 @@
+class MergeSort{
+    static int[] divide(int [] array){
+        return array;
+    }
+
+    static int[] merge(int [] array){
+        return array;
+    }
+
+    static void mergeSort(int [] array){
+
+    }
+
+    public static void main(String[] args) {
+        int [] array = {38,25,21,26,28};
+        MergeSort obj = new MergeSort();
+    }
+}
